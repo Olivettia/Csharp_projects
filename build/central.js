b879541e9ff8856cc638e363e2dc491bf06ecfe3
Olivettia <97970683+Olivettia@users.noreply.github.com>
2021-07-15 16:54:53 +0000

    USB: serial: pl2303: add device id for ztek device

--- build/central.js
+++ build/central.js
@@ -1,4 +1,13 @@
 (function() {
     var Central = {};
     if (typeof module != 'undefined' && module.exports) {
-        module.exports = Ce
+        module.exports = Central;
+    } else if (typeof YUI != 'undefined' && YUI.add) {
+        YUI.add('central', function(Y) {
+            Y.Central = Central;
+        }, '1.0.6', {
+            requires: []
+        })
+    } else if (typeof window == 'object') {
+        window.Central = Central;
+    } else 
