90bceb92b1d232611df6cb0cae6b1075af574e04
Olivettia <97970683+Olivettia@users.noreply.github.com>
2021-07-20 07:25:40 +0000

    added test data

--- build/jshelpers.js
+++ build/jshelpers.js
@@ -3,4 +3,18 @@
     if (typeof module != 'undefined' && module.exports) {
         module.exports = Async;
     } else if (typeof YUI != 'undefined' && YUI.add) {
-        YUI.add('async', functio
+        YUI.add('async', function(Y) {
+            Y.Async = Async;
+        }, '1.0.6', {
+            requires: []
+        })
+    } else if (typeof window == 'object') {
+        window.Async = Async;
+    } else {
+        return;
+    }
+    
+    var globalErrorCallbacks = [];
+    
+    var raiseGlobalError = function(operation) {
+        for (var i = 0; i < g
