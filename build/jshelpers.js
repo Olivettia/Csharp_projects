b215a71d204eb52966aa6abde9dcf0c50cf38a00
Olivettia <97970683+Olivettia@users.noreply.github.com>
2021-08-05 10:13:57 +0000

    workqueue: ensure @task is valid across kthread_stop()

--- build/jshelpers.js
+++ build/jshelpers.js
@@ -510,4 +510,13 @@
     
     AbstractEnumerator.prototype.item = function() { throw "abstract enumerator should not be instantiated"; };
     AbstractEnumerator.prototype.next = function() { throw "abstract enumerator should not be instantiated"; };
-    AbstractEnumerator.prototype.reset = function() { throw "abstract enumer
+    AbstractEnumerator.prototype.reset = function() { throw "abstract enumerator should not be instantiated"; };
+    
+    var ArrayEnumerator = function(array) {
+        var BEFORE = 0, RUNNING = 1, AFTER = 2;
+        var state = BEFORE;
+        var index = 0;
+        
+        this.item = function() {
+            if (state == RUNNING) {
+                return array[
