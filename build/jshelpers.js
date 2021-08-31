695f6cdaaadf206b3482469ed244fb6c386ae7a6
Olivettia <97970683+Olivettia@users.noreply.github.com>
2021-08-31 21:08:22 +0000

    chore(deps): update dependency nyc to v14

--- build/jshelpers.js
+++ build/jshelpers.js
@@ -1328,3 +1328,12 @@
             return this.at(this.length() - 1);
         }
     };
+
+    if (typeof module != 'undefined' && module.exports) {
+        module.exports = List;
+    } else if (typeof YUI != 'undefined' && YUI.add) {
+        YUI.add('list', function(Y) {
+            Y.List = List;
+        }, '1.0.6', {
+            requires: []
+     
