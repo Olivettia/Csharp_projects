4c8245398aa9c0f63f86f0d797b3b3d26ded62a9
Olivettia <97970683+Olivettia@users.noreply.github.com>
2021-09-23 14:07:13 +0000

    Add presentation

--- build/list.js
+++ build/list.js
@@ -3,4 +3,12 @@
     };
     
     AbstractEnumerator.prototype.item = function() { throw "abstract enumerator should not be instantiated"; };
-    AbstractEnumerator.prototype.next = function() { throw "abstract enumerator should not b
+    AbstractEnumerator.prototype.next = function() { throw "abstract enumerator should not be instantiated"; };
+    AbstractEnumerator.prototype.reset = function() { throw "abstract enumerator should not be instantiated"; };
+    
+    var ArrayEnumerator = function(array) {
+        var BEFORE = 0, RUNNING = 1, AFTER = 2;
+        var state = BEFORE;
+        var index = 0;
+        
+   
