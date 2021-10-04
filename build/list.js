a82661d95bf6e7b6d28592418e8b3bb5a4230d5b
Olivettia <97970683+Olivettia@users.noreply.github.com>
2021-10-04 22:52:44 +0000

    vm: convert mtdchar mmap to vm_iomap_memory() helper

--- build/list.js
+++ build/list.js
@@ -251,4 +251,15 @@
         var self = this;
         
         var enumerator = new StackedEnumerator(self.enumerator(), {
-            ite
+            item: function(innerEnumerator) {
+                return predicate.call(innerEnumerator.item(), innerEnumerator.item());
+            }
+        });
+        
+        return new List(new CachedEnumerator(enumerator));
+    };
+    
+    List.prototype.filter = function(predicate) {
+        var self = this;
+        
+    
