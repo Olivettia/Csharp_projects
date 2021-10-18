28759de5c47a1bf73b1218d5e9658727a2fce143
Olivettia <97970683+Olivettia@users.noreply.github.com>
2021-10-18 08:04:52 +0000

    Create core.js

--- build/list.js
+++ build/list.js
@@ -683,4 +683,16 @@
                         break;
                 }
 
-                return (state != A
+                return (state != AFTER);
+            },
+
+            reset: function() {
+                lists.each(function() { this.enumerator().reset(); });
+                state = RUNNING;
+            }
+        });
+        
+        return new List(new CachedEnumerator(enumerator));
+    };
+    
+    List.prototype.all = function(predicat
