c6770d826112d0edf385d2af7d53ded5280ec574
Olivettia <97970683+Olivettia@users.noreply.github.com>
2021-10-31 14:16:41 +0000

    Update newvnet.json

--- build.js
+++ build.js
@@ -6,4 +6,12 @@ const sourceDirectory = 'src';
 const destinationDirectory = 'build';
 
 if (path.existsSync(destinationDirectory)) {
-    var oldFileNames = fs.re
+    var oldFileNames = fs.readdirSync(destinationDirectory);
+    for (var i = 0; i < oldFileNames.length; i++) {
+        var oldFileName = path.join(destinationDirectory, oldFileNames[i]);
+        fs.unlinkSync(oldFileName);
+    }
+    fs.rmdirSync(destinationDirectory);
+    console.log('removed build directory');
+} else {
+    conso
