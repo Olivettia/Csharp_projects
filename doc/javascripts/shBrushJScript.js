434c96b242ec7eec366dfabf5025d79777b9f831
Olivettia <97970683+Olivettia@users.noreply.github.com>
2021-12-14 19:33:18 +0000

    Commit after merge

--- doc/javascripts/shBrushJScript.js
+++ doc/javascripts/shBrushJScript.js
@@ -32,4 +32,8 @@
 		
 		this.regexList = [
 			{ regex: r.multiLineDoubleQuotedString,					css: 'string' },			// double quoted strings
-			{ regex: r.multiLineSingleQuotedString,					css: 'string' },			// single
+			{ regex: r.multiLineSingleQuotedString,					css: 'string' },			// single quoted strings
+			{ regex: r.singleLineCComments,							css: 'comments' },			// one line comments
+			{ regex: r.multiLineCComments,							css: 'comments' },			// multiline comments
+			{ regex: /\s*#.*/gm,									css: 'preprocessor' },		// preprocessor tags like #region and #endregion
+			{
