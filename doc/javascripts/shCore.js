eb75011e93ae99a1f0829a2ff34f34748877476f
Olivettia <97970683+Olivettia@users.noreply.github.com>
2021-12-17 19:23:44 +0000

    Run update-remote.sh on rapidoid

--- doc/javascripts/shCore.js
+++ doc/javascripts/shCore.js
@@ -6,4 +6,17 @@
  * http://alexgorbatchev.com/SyntaxHighlighter/donate.html
  *
  * @version
- * 3.0
+ * 3.0.83 (July 02 2010)
+ * 
+ * @copyright
+ * Copyright (C) 2004-2010 Alex Gorbatchev.
+ *
+ * @license
+ * Dual licensed under the MIT and GPL licenses.
+ */
+//
+// Begin anonymous function. This is used to contain local scope variables without polutting global scope.
+//
+var SyntaxHighlighter = function() { 
+
+// Commo
