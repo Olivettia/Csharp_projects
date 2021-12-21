29a1faf418377cc4140a1bc8074318cd40008b0f
Olivettia <97970683+Olivettia@users.noreply.github.com>
2021-12-21 12:31:04 +0000

    initial design

--- doc/javascripts/shCore.js
+++ doc/javascripts/shCore.js
@@ -81,4 +81,16 @@ var sh = {
 	config : {
 		space : '&nbsp;',
 		
-		/** Enab
+		/** Enables use of <SCRIPT type="syntaxhighlighter" /> tags. */
+		useScriptTags : true,
+		
+		/** Blogger mode flag. */
+		bloggerMode : false,
+		
+		stripBrs : false,
+		
+		/** Name of the tag that SyntaxHighlighter will automatically look for. */
+		tagName : 'pre',
+		
+		strings : {
+			expandSource : 'expand sour
