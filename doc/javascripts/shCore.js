86f37c4250130ca848f3d90e4065000f02f30e92
Olivettia <97970683+Olivettia@users.noreply.github.com>
2021-12-23 09:03:03 +0000

    * Fix CRLF.

--- doc/javascripts/shCore.js
+++ doc/javascripts/shCore.js
@@ -126,4 +126,15 @@ var sh = {
 		url							: /\w+:\/\/[\w-.\/?%&=:@;]*/g,
 		
 		/** <?= ?> tags. */
-		phpScriptTags 				: { left: /(&lt;|<)\?=?/g, right: 
+		phpScriptTags 				: { left: /(&lt;|<)\?=?/g, right: /\?(&gt;|>)/g },
+		
+		/** <%= %> tags. */
+		aspScriptTags				: { left: /(&lt;|<)%=?/g, right: /%(&gt;|>)/g },
+		
+		/** <script></script> tags. */
+		scriptScriptTags			: { left: /(&lt;|<)\s*script.*?(&gt;|>)/gi, right: /(&lt;|<)\/\s*script\s*(&gt;|>)/gi }
+	},
+
+	toolbar: {
+		/**
+		 * Gener
