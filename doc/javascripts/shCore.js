ef593ca605deb0aff7b752352113637fae6609ac
Olivettia <97970683+Olivettia@users.noreply.github.com>
2022-01-22 16:05:15 +0000

    v2.8

--- doc/javascripts/shCore.js
+++ doc/javascripts/shCore.js
@@ -910,4 +910,20 @@ function processSmartTabs(code, tabSize)
 };
 
 /**
- * Performs various 
+ * Performs various string fixes based on configuration.
+ */
+function fixInputString(str)
+{
+	var br = /<br\s*\/?>|&lt;br\s*\/?&gt;/gi;
+	
+	if (sh.config.bloggerMode == true)
+		str = str.replace(br, '\n');
+
+	if (sh.config.stripBrs == true)
+		str = str.replace(br, '');
+		
+	return str;
+};
+
+/**
+ * Removes all white space at the beg
