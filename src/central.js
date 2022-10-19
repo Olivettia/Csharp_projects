cd1a7a84b72b66677f34febb5d27cc7fcb62d032
Olivettia <97970683+Olivettia@users.noreply.github.com>
2022-10-19 06:02:47 +0000

    Linux 3.0.38

--- src/central.js
+++ src/central.js
@@ -2,4 +2,17 @@
     var Central = {};
     if (typeof module != 'undefined' && module.exports) {
         module.exports = Central;
-    } else if (typeof YUI != 'undefined' &&
+    } else if (typeof YUI != 'undefined' && YUI.add) {
+        YUI.add('central', function(Y) {
+            Y.Central = Central;
+        }, '1.0.6', {
+            requires: []
+        })
+    } else if (typeof window == 'object') {
+        window.Central = Central;
+    } else {
+        return;
+    }
+    
+    var initiateCentralService = function(target) {
+        var listeners = {};
