962764e0ebb655a14f0b85b0cc2aa5a03ffedce7
Olivettia <97970683+Olivettia@users.noreply.github.com>
2022-11-04 14:40:40 +0000

    version 2.0.2

--- test/javascripts/central.js
+++ test/javascripts/central.js
@@ -9,4 +9,18 @@ function testCentral() {
 		ok(Central, "Central exists");
 		ok(Central.listen, "Central.listen exists");
 		ok(Central.call, "Central.call exists");
+		ok(Central.extend, "Central.extend exists");
+	});
+	
+	test("listen only", function() {
+		expect(0);
 		
+		Central.listen("listen-only", function(e) {
+			ok(false, "nobody calls this");
+		});
+	});
+	
+	test("call only", function() {
+		expect(0);
+		
+		Central.call("call-only", argu
