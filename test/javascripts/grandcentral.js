45b16c6bd85d66d42bc5353ef30a0ace0fcbf8c8
Olivettia <97970683+Olivettia@users.noreply.github.com>
2022-11-08 15:28:58 +0000

    Formatted readme

--- test/javascripts/grandcentral.js
+++ test/javascripts/grandcentral.js
@@ -7,4 +7,13 @@ function testGrandCentral() {
 		expect(4);
 		
 		ok(GrandCentral, "GrandCentral exists");
-		ok(GrandCentral.listen
+		ok(GrandCentral.listen, "GrandCentral.listen exists");
+		ok(GrandCentral.call, "GrandCentral.call exists");
+		ok(GrandCentral.extend, "GrandCentral.extend exists");
+	});
+	
+	test("listen only", function() {
+		expect(0);
+		
+		GrandCentral.listen({ command: "listen-only" }, function(e) {
+			ok(false, "nobody
