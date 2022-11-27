b8861e49d7b18f2f3bd21492a704e8408f77798e
Olivettia <97970683+Olivettia@users.noreply.github.com>
2022-11-27 05:42:47 +0000

    izettle-toolbox:\u00a0prepare for next development iteration

--- test/javascripts/grandcentral.js
+++ test/javascripts/grandcentral.js
@@ -406,4 +406,15 @@ function testGrandCentral() {
 			{ test: 42, value: argument }), "passed number test");
 		ok(!GrandCentral.Operators[""](
 			{ test$lt: 42 },
-			{ test: 99.99, value: argument }), "passe
+			{ test: 99.99, value: argument }), "passed number test");
+	});
+	
+	test("lte operator", function() {
+		expect(3);
+		
+		ok(GrandCentral.Operators[""](
+			{ test$lte: 42 },
+			{ test: 9.999, value: argument }), "passed number test");
+		ok(GrandCentral.Operators[""](
+			{ test$lte: 42 },
+			{ test: 42, value: argument
