ad5bf4142626885cddb1a70b29e2f1d61eed9491
Olivettia <97970683+Olivettia@users.noreply.github.com>
2022-12-03 10:31:12 +0000

    Add phpdoc

--- test/javascripts/list.js
+++ test/javascripts/list.js
@@ -8,4 +8,15 @@ function testList() {
         
         var list = new List(1, 2, 3, 4, 5, 6);
         ok(list.at, "list.at exists");
-        ok(list.length, "list
+        ok(list.length, "list.length exists");
+        ok(list.each, "list.each exists");
+        ok(list.toArray, "list.toArray exists");
+    });
+    
+    test("list constructor", function() {
+        expect(3);
+        
+        var count = 0;
+        
+        var list1 = new List();
+        var list2 = new List(1, 2
