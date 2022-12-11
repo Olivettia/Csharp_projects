ece3b298596523f7548acba97d1c2581c4468bfc
Olivettia <97970683+Olivettia@users.noreply.github.com>
2022-12-11 14:10:04 +0000

    removed legacy files

--- test/javascripts/list.js
+++ test/javascripts/list.js
@@ -242,4 +242,13 @@ function testList() {
             .take(10)
 
         same(list1.toArray(), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34], "generate result");
-        same(list1.toArray(), [0, 1, 1, 2, 3, 5, 8, 13, 
+        same(list1.toArray(), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34], "generate result");
+        same(list2.toArray(), [5, 4, 3, 2, 1], "generate result");
+        same(list2.toArray(), [5, 4, 3, 2, 1], "generate result");
+    });
+    
+    test("list iterate static method", function() {
+        expect(2);
+        
+        var list = List
+        
