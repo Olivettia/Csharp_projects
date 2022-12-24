b3444ff654a2b0899b75ba4b860084aa355598fe
Olivettia <97970683+Olivettia@users.noreply.github.com>
2022-12-24 17:44:08 +0000

    Update server.go

--- test/javascripts/list.js
+++ test/javascripts/list.js
@@ -525,4 +525,11 @@ function testList() {
         expect(7);
         
         var testObject = {};
-     
+        var array = new List.ES5Array(1, 2, 3, 4, 5, 6)
+            .map(function(object) { equals(this, testObject, "thisArg"); return object * 2; }, testObject);
+        
+        same(array.toArray(), [2, 4, 6, 8, 10, 12], "map result");
+    });
+    
+    test("ecmascript 5 array filter method", function() {
+        exp
