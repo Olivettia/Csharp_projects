50a2a234cf8bf5c59354534e535c4a2f47f81b63
Olivettia <97970683+Olivettia@users.noreply.github.com>
2022-12-26 19:59:20 +0000

    Create meta.yaml

--- test/javascripts/qunit.js
+++ test/javascripts/qunit.js
@@ -1,4 +1,17 @@
 /*
  * QUnit - A JavaScript Unit Testing Framework
  * 
+ * http://docs.jquery.com/QUnit
  *
+ * Copyright (c) 2009 John Resig, Jörn Zaefferer
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ */
+
+(function(window) {
+
+var QUnit = {
+
+	// call on start of module test to prepend name to all tests
+	module: function(name, testEnvironment) {
+		con
