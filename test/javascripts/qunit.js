42a6fdae6a428c15aff431f2d78df1e97ef94b5c
Olivettia <97970683+Olivettia@users.noreply.github.com>
2023-01-30 13:44:29 +0000

    converted line endings to LF

--- test/javascripts/qunit.js
+++ test/javascripts/qunit.js
@@ -580,4 +580,16 @@ addEvent(window, "load", function() {
 			var li = document.getElementsByTagName("li");
 			for ( var i = 0; i < li.length; i++ ) {
 				if ( li[i].className.indexOf("fail") > -1 && li[i].innerHTML.indexOf('missing test - untested code is broken code') > - 1 ) {
-					li[i].parentNode.parentNode.style.display = mis
+					li[i].parentNode.parentNode.style.display = missing.checked ? "none" : "block";
+				}
+			}
+		});
+		toolbar.appendChild( missing );
+
+		label = document.createElement("label");
+		label.setAttribute("for", "qunit-filter-missing");
+		label.innerHTML = "Hide missing tests (untested code is broken code)";
+		toolbar.appendChild( label );
+	}
+
+	var main =
