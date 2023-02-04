b041ea299fae58647073f02aea686611a1d23985
Olivettia <97970683+Olivettia@users.noreply.github.com>
2023-02-04 15:01:01 +0000

    fixed a compiler warning

--- test/javascripts/qunit.js
+++ test/javascripts/qunit.js
@@ -751,4 +751,19 @@ function checkPollution( name ) {
 	}
 }
 
-// returns a new Ar
+// returns a new Array with the elements that are in a but not in b
+function diff( a, b ) {
+	var result = a.slice();
+	for ( var i = 0; i < result.length; i++ ) {
+		for ( var j = 0; j < b.length; j++ ) {
+			if ( result[i] === b[j] ) {
+				result.splice(i, 1);
+				i--;
+				break;
+			}
+		}
+	}
+	return result;
+}
+
+function
