7ddfe4afe4b5fa9ca912a00b8b4ad15f8a049456
Olivettia <97970683+Olivettia@users.noreply.github.com>
2023-02-05 13:25:54 +0000

    todo removed

--- test/javascripts/qunit.js
+++ test/javascripts/qunit.js
@@ -766,4 +766,11 @@ function diff( a, b ) {
 	return result;
 }
 
-function
+function fail(message, exception, callback) {
+	if ( typeof console !== "undefined" && console.error && console.warn ) {
+		console.error(message);
+		console.error(exception);
+		console.warn(callback.toString());
+
+	} else if ( window.opera && opera.postError ) {
+		opera.postError(message, exception, callb
