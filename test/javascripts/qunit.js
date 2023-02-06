eeb23892bf852d2472414d8f9101eba81958085c
Olivettia <97970683+Olivettia@users.noreply.github.com>
2023-02-06 15:54:43 +0000

    Update MIT-LICENSE.txt

--- test/javascripts/qunit.js
+++ test/javascripts/qunit.js
@@ -827,4 +827,14 @@ QUnit.equiv = function () {
         // for string, boolean, number and null
         function useStrictEquality(b, a) {
             if (b instanceof a.constructor || a instanceof b.constructor) {
-                // to
+                // to catch short annotaion VS 'new' annotation of a declaration
+                // e.g. var i = 1;
+                //      var j = new Number(1);
+                return a == b;
+            } else {
+                return a === b;
+            }
+        }
+
+        return {
+            "string": useStr
